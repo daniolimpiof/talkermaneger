d92d46370c6bd58c9062b611060b0c28b361ee93
Danielly Olimpio <danielly.fundcheller@gmail.com>
2022-06-21 14:38:26 -0300

    heavy_check_mark :heavy_check_mark:	requisição GET para /talker/:id

--- index.js
+++ index.js
@@ -25,3 +25,13 @@ app.get('/talker', async (_req, res) => {
   res.status(200).json(talkers);
  // console.log('Req 1: GET /talker');
 });
+
+// requisição GET para /talker/:id
+app.get('/talker/:id', async (req, res) => {
+  const { id } = req.params;
+  const talker = await readContentFile(PATH_FILE);
+  const talkerInfo = talker.find((idNumb) => idNumb.id === Number(id));
+  if (!talkerInfo) return res.status(404).json({ message: 'Pessoa palestrante não encontrada' });
+
+  res.status(200).json(talkerInfo);
+});
